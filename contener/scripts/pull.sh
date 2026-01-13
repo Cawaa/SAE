1f0c167a533570fafdcdbec3a5224a3c8e98f746
elric <elricdufresne@etu.univ-nantes.fr>
2026-01-13 14:24:37 +0100

    pb resolu

--- contener/scripts/pull.sh
+++ contener/scripts/pull.sh
@@ -1,1 +1,2 @@
+# Récupère les modifications du conteneur vers le dossier local
 podman cp php:/var/www/html/. data/
--- contener/scripts/push.sh
+++ contener/scripts/push.sh
@@ -1,4 +1,3 @@
-
-
+# Copie le dossier data local vers le serveur web du conteneur
 podman cp data/. php:/var/www/html/
 podman exec php chown -R www-data:www-data /var/www
